718a94e057be522d99837501cd6daee1b1d21a9f
Daniil_Teslov <dteslov@gmail.com>
2023-06-17 12:05:46 +0600

    First commit

---
+++ GemsLabs/Degree.cs
@@ -0,0 +1,12 @@
+﻿namespace Task0
+{
+    internal class Degree
+    {
+        public static string TransformDegree(int temp, char param)
+        {
+            return Char.ToUpper(param) == 'C' 
+                ? Math.Round(temp * 9.0 / 5 + 32, 2).ToString() + "F"
+                : Math.Round((temp - 32) * 5.0 / 9).ToString() + "C";
+        }
+    }
+}
---
+++ GemsLabs/Year.cs
@@ -0,0 +1,10 @@
+﻿namespace Task0
+{
+    public class Year
+    {
+        public static bool CheckYear(int year)
+        {
+            return year >= 1 && (year % 4 == 0 && year % 100 != 0 || year % 400 == 0);
+        }
+    }
+}
