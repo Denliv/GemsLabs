c95f1a28f887ff44449ebcaa062c6924fe8307dd
Daniil_Teslov <dteslov@gmail.com>
2023-08-14 16:28:06 +0600

    Task8:MyLinq corrections

--- MyExtensions/MyLinq.cs
+++ MyExtensions/MyLinq.cs
@@ -29,31 +29,26 @@ public static class MyLinq
 
     public static IEnumerable<T> Where<T>(this IEnumerable<T>? collection, Predicate<T> cond)
     {
-        var temp = new List<T>();
+        var temp = new LinkedList<T>();
         if (collection == null) return temp;
         var enumerable = collection.ToArray();
         if (enumerable.Length == 0) return temp;
         foreach (var i in enumerable)
         {
-            if (i != null && cond.Invoke(i)) temp.Add(i);
+            if (i != null && cond.Invoke(i)) temp.AddLast(i);
         }
 
         return temp;
     }
 
-    public static bool Any<T>(this IEnumerable<T>? collection)
-    {
-        return collection != null && Enumerable.Any(collection);
-    }
-
-    public static bool Any<T>(this IEnumerable<T>? collection, Predicate<T> cond)
+    public static bool Any<T>(this IEnumerable<T>? collection, Predicate<T>? cond = null)
     {
         if (collection == null) return false;
         var enumerable = collection.ToArray();
         if (enumerable.Length == 0) return false;
         foreach (var i in enumerable)
         {
-            if (i != null && cond.Invoke(i)) return true;
+            if (i != null && (cond?.Invoke(i) ?? true)) return true;
         }
 
         return false;
